3aeaa4ac22213166b24ef60763c4bdfd1bbe9a3b
Shelter <94851109+shelte1221@users.noreply.github.com>
2022-08-25 15:10:40 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,6 @@
-from typing import ClassVar, Sequence
-from aiohttp.http import SERVER_SOFTWARE
+
 import discord
-from discord import message
-from discord import user
-from discord import player
-from discord import channel
-from discord.channel import VoiceChannel
-from discord.client import Client
-from discord.enums import ExpireBehaviour
-from discord.ext import commands
-from discord.utils import sleep_until
-import time
-import io
-import aiohttp
-import pickle
-from datetime import datetime
-import pytz
-import os
+
 
 client = commands.Bot(command_prefix = '..')
 
