feb0c9565be5c762ff1d23e43f660172f82f60e5
Shelterin <giangtruongsu@gmail.com>
2025-05-30 23:55:08 +0700

    fix bug

--- README.md
+++ README.md
@@ -1,29 +1,77 @@
-# Hosting your discord.py bot on Heroku
-### What are the prerequisites for this guide?
-You must have an account for Discord [[Link](https://discordapp.com/developers/applications/)], GitHub [[Link](https://github.com/join)] , and Heroku [[Link](https://signup.heroku.com/)].
+# Discord Message Logger Bot
 
-### How do I create a bot and get a bot token?
-* Create an application in the developer portal by clicking [here](https://discordapp.com/developers/applications/)
-* Open up your new application and click 'Add Bot' under the Bot settings to create your bot.
-* After creating the bot, click the 'Copy' button under the title Token. Take note of your token as you will need it later.
+Bot để log các tin nhắn bị xóa và chỉnh sửa trong Discord server.
 
-### How to fork the repository and set it up to work with Heroku?
-* Fork a copy of this repository by clicking the 'Fork' on the upper right-hand.
-* Create an application for Heroku by clicking [here](https://dashboard.heroku.com/new-app).
-* Under 'Deploy', do the following:
-  * Deployment Method => Connect your GitHub
-  * App connected to GitHub => Search for the forked repository
-  * Automatic Deploy => Enable Automatic Deploy (to redeploy after every commit)
-* Under 'Settings', click on 'Reveal Config Vars' and enter the following:
-  * KEY => DISCORD_TOKEN
-  * VALUE => (Enter the bot token that you copied from the developer portal)
-  * Click the 'Add' button after entering all of this information.
-* Under 'Resources', do the following:
-  * Click on the 'Pencil' icon.
-  * Switch the worker from off to on.
-  * Click 'Confirm' to finalize the decision.
-  * NOTE: You are allocated 550 free Dyno hours, which will not last the entire month. However, if you provide a credit card to verify your identity, you are given an additional 450 hours, which will allow your bot to run indefinitely.
+## Cài đặt
 
-# TO-DO LIST
-* Revise the tutorial to be easier to understand for any user.
-* Add some images for following along with the process.
+1. Cài đặt Python 3.8 hoặc cao hơn
+2. Cài đặt các thư viện cần thiết:
+```bash
+pip3 install -r requirements.txt
+```
+
+3. Tạo file `.env` với nội dung:
+```
+DISCORD_TOKEN=your_bot_token_here
+```
+
+## Bảo mật
+
+⚠️ **QUAN TRỌNG**: Không bao giờ push các file sau lên GitHub:
+- `.env` (chứa token bot)
+- `server_channels.json` (chứa cấu hình server)
+
+Các file này đã được thêm vào `.gitignore` để tránh vô tình push lên.
+
+## Cấu hình Bot
+
+1. Tạo bot mới tại [Discord Developer Portal](https://discord.com/developers/applications)
+2. Bật các Intents sau trong Discord Developer Portal:
+   - MESSAGE CONTENT INTENT
+   - SERVER MEMBERS INTENT
+
+3. Mời bot vào server với link sau (thay YOUR_CLIENT_ID bằng Client ID của bot):
+```
+https://discord.com/api/oauth2/authorize?client_id=YOUR_CLIENT_ID&permissions=8&scope=bot
+```
+
+## Quyền cần thiết
+
+Bot cần các quyền sau để hoạt động:
+
+### Quyền cơ bản
+- ✅ Send Messages
+- ✅ Read Message History
+- ✅ View Channels
+- ✅ Embed Links
+- ✅ Attach Files
+- ✅ Read Messages/View Channels
+
+### Quyền cho lệnh admin
+- ✅ Administrator (cho lệnh setlog và removelog)
+
+## Cách sử dụng
+
+1. Set channel log:
+```
+..setlog #channel-name
+```
+hoặc
+```
+..setlog
+```
+(để set channel hiện tại làm channel log)
+
+2. Xóa channel log:
+```
+..removelog
+```
+
+## Tính năng
+
+- Log tin nhắn bị xóa
+- Log tin nhắn bị chỉnh sửa
+- Log file đính kèm
+- Hỗ trợ nhiều server
+- Tự động lưu cấu hình
+- Thông báo lỗi chi tiết
--- main.py
+++ main.py
@@ -1,134 +1,267 @@
-
 import discord
-from discord import message
-from discord import user
-from discord import player
-from discord import channel
-from discord.channel import VoiceChannel
-from discord.client import Client
-from discord.enums import ExpireBehaviour
 from discord.ext import commands
-from discord.utils import sleep_until
-# import time
-# import io
-# import aiohttp
-# import pickle
-# from datetime import datetime
-# import pytz
-# import os
+import datetime
+import pytz
+import io
+import aiohttp
+import os
+from dotenv import load_dotenv
+import json
+import traceback
 
-client = commands.Bot(command_prefix = '..')
+# Load environment variables
+load_dotenv()
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+client = commands.Bot(command_prefix='..', intents=intents)
+
+# Dictionary to store server logging channels
+# Format: {server_id: logging_channel_id}
+SERVER_LOGGING_CHANNELS = {}
+
+def load_server_channels():
+    try:
+        with open('server_channels.json', 'r') as f:
+            data = json.load(f)
+            # Convert string IDs to integers
+            return {int(k): int(v) for k, v in data.items()}
+    except FileNotFoundError:
+        print("server_channels.json not found. Creating new file...")
+        return {}
+    except json.JSONDecodeError:
+        print("Error reading server_channels.json. Creating new file...")
+        return {}
+    except Exception as e:
+        print(f"Error loading server channels: {e}")
+        return {}
+
+def save_server_channels():
+    try:
+        # Convert integer IDs to strings for JSON storage
+        data = {str(k): str(v) for k, v in SERVER_LOGGING_CHANNELS.items()}
+        with open('server_channels.json', 'w') as f:
+            json.dump(data, f, indent=4)
+    except Exception as e:
+        print(f"Error saving server channels: {e}")
+
+# Load saved server channels
+SERVER_LOGGING_CHANNELS = load_server_channels()
 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print(f'We have logged in as {client.user}')
+    print("\nConnected to servers:")
+    for guild in client.guilds:
+        print(f"- {guild.name} (ID: {guild.id})")
+        if guild.id not in SERVER_LOGGING_CHANNELS:
+            print(f"  Warning: No logging channel set for this server!")
+        else:
+            channel = client.get_channel(SERVER_LOGGING_CHANNELS[guild.id])
+            if channel:
+                print(f"  Logging channel: #{channel.name}")
+            else:
+                print(f"  Error: Logging channel {SERVER_LOGGING_CHANNELS[guild.id]} not found!")
 
 @client.event
-async def on_message(ctx):
-    if ctx.author == client.user:
-        return
-    await client.process_commands(ctx)
+async def on_error(event, *args, **kwargs):
+    print(f"Error in {event}:")
+    traceback.print_exc()
 
-# res = { 
-#     952847230648348722 : 952848925226840094, 
-#     893119319167344700 : 924261082825130026, # arm
-#       946775239235346492 : 946778374053961738,
-#       # 897472040511111180 : 918705277098930216 #serve minh
-#     }
-# tem = {897472040511111180 : 918705277098930216,
-#                 734325804007161878 : 734325804443631648}
-# Bot_free = {"Mudae#0807", "Ayana#8911"}
-# channel_free = {"885546099752968232"}
+@client.command()
+@commands.has_permissions(administrator=True)
+async def setlog(ctx, channel: discord.TextChannel = None):
+    """Set the logging channel for this server"""
+    try:
+        # If no channel is specified, use the current channel
+        if channel is None:
+            channel = ctx.channel
 
-# @client.event
-# async def on_message_delete(ctx):
-#     channel = client.get_channel(res[ctx.guild.id])
-#     if str(ctx.author) not in Bot_free and str(ctx.channel) not in channel_free:
-#         try:
-#             ag = content=ctx.attachments[0].url
-#             a = "```arm" + "\n"
-#             a += str(ctx.author) + " has delete a File" + "\n"
-#             a += "File: " + ag[77:len(ag)] + "\n"
-#             a += "Channel: " + str(ctx.channel) + "\n"
-#             a += "Time: " + time_string() + "\n"
-#             a += "This is the file that he has delete!"  + "```"
-#             # tai anh len discord
-#             async with aiohttp.ClientSession() as session:
-#                 async with session.get(ag) as resp:
-#                     data = io.BytesIO(await resp.read())
-#                     msg = await channel.send(a)
-#                     await msg.reply(file=discord.File(data, ag[77:len(ag)]))
-#         except:
-#             try:
-#                 embeds = ctx.embeds
-#                 msg = await channel.send(embed=embeds[0])
-#                 await msg.reply('This is the embed that ' + str(ctx.author) + 
-#                 ' has delete' + ' in Channel: ' + str(ctx.channel) + ' | Time: ' + time_string())
-#             except:
-#               try:
-#                   a = "```arm" + "\n"
-#                   a += str(ctx.author) + " has delete a message" + "\n"
-#                   a += "Message: " + str(ctx.content) + "\n"
-#                   a += "Channel: " + str(ctx.channel) + "\n"
-#                   a += "Time: " + time_string() + "\n"
-#                   a += "This is the message that he has delete!"  + "```"
-#                   await channel.send(a)
-#                   b = check_ping(ctx.content)
-#                   if(b != 0):
-#                     await ctx.channel.send("<@" + str(ctx.author.id) + ">" +" m thích tag" +  b + "à t xiên giờ ")
-#                     await ctx.channel.send("?xien")
-#               except:
-#                 msg = await channel.send(str(ctx.author) + ' has delete ' + ' in Channel: ' + str(ctx.channel) + ' | Time: ' + time_string())
-#                 a = write_file(ctx.content)
-#                 b = check_ping(a)
-#                 if(b != 0):
-#                   await ctx.channel.send("<@" + str(ctx.author.id) + ">" +" m thích tag" +  b + "à t xiên giờ ")
-#                   await ctx.channel.send("?xien")
-#                 await msg.reply(file=discord.File("text.txt"))
+        # Check if the bot has permission to send messages in the channel
+        if not channel.permissions_for(ctx.guild.me).send_messages:
+            await ctx.send("❌ I don't have permission to send messages in that channel!")
+            return
 
+        # Check if the bot has permission to read message history
+        if not channel.permissions_for(ctx.guild.me).read_message_history:
+            await ctx.send("❌ I don't have permission to read message history in that channel!")
+            return
 
-# def check_ping(string):
-#     string = str(string)
-#     a = " "
-#     check = 0
-#     if(string.find('@everyone',check) != -1): 
-#         check = string.find('@everyone',check) + 1
-#         a += "everyone "
-#     check = 0
-#     if(string.find('@here',check) != -1):
-#         check = string.find('@here',check) + 1
-#         a+= "here "
-#     if(a == " "):
-#         return 0
-#     else:
-#         return a
+        # Set the logging channel
+        SERVER_LOGGING_CHANNELS[ctx.guild.id] = channel.id
+        save_server_channels()
 
-# @client.event
-# async def on_message_edit(ctx_bef, ctx_aft):
-#     channel = client.get_channel(res[ctx_bef.guild.id])
-#     if str(ctx_bef.author) not in Bot_free and str(ctx_bef.content) != str(ctx_aft.content) and str(ctx_bef.author) not in "ProBot ✨#5803" and str(ctx_bef.author) not in "FredBoat♪♪#7284"  and str(ctx_bef.channel) not in channel_free:
-#         a = "```arm" + "\n"
-#         a += str(ctx_bef.author) + " has edit a message" + "\n"
-#         a += "Old: " + str(ctx_bef.content) + "\n"
-#         a += "New: " + str(ctx_aft.content) + "\n"
-#         a += "Channel: " + str(ctx_bef.channel) + "\n"
-#         a += "Time: " + time_string() + "\n"
-#         a += "This is the message that he has edit!"  + "```"
-#         try:
-#           await channel.send(a)
-#         except:
-#           await channel.send(str(ctx_bef.author) + ' has edit ' + 'in ' + time_string())
-#           await channel.send("```arm" + "\n" + "Old:" + "```")
-#           write_file(ctx_bef.content)
-#           await channel.send(file=discord.File("text.txt"))
-#           await channel.send("```arm" + "\n" + "New:" + "```")
-#           write_file(ctx_aft.content)
-#           await channel.send(file=discord.File("text.txt"))
+        # Send confirmation message
+        embed = discord.Embed(
+            title="✅ Logging Channel Set",
+            description=f"Logging channel has been set to {channel.mention}",
+            color=discord.Color.green()
+        )
+        embed.add_field(name="Server", value=ctx.guild.name, inline=True)
+        embed.add_field(name="Channel", value=channel.name, inline=True)
+        await ctx.send(embed=embed)
+
+        # Send a test message to the logging channel
+        test_embed = discord.Embed(
+            title="Test Message",
+            description="This is a test message to confirm the logging channel is working.",
+            color=discord.Color.blue()
+        )
+        await channel.send(embed=test_embed)
+
+    except Exception as e:
+        print(f"Error in setlog command: {e}")
+        traceback.print_exc()
+        error_embed = discord.Embed(
+            title="❌ Error",
+            description=f"An error occurred while setting the logging channel: {str(e)}",
+            color=discord.Color.red()
+        )
+        await ctx.send(embed=error_embed)
+
+@client.command()
+@commands.has_permissions(administrator=True)
+async def removelog(ctx):
+    """Remove the logging channel for this server"""
+    try:
+        if ctx.guild.id in SERVER_LOGGING_CHANNELS:
+            del SERVER_LOGGING_CHANNELS[ctx.guild.id]
+            save_server_channels()
+            embed = discord.Embed(
+                title="✅ Logging Channel Removed",
+                description="Logging channel has been removed for this server.",
+                color=discord.Color.green()
+            )
+            await ctx.send(embed=embed)
+        else:
+            embed = discord.Embed(
+                title="ℹ️ No Logging Channel",
+                description="No logging channel was set for this server.",
+                color=discord.Color.blue()
+            )
+            await ctx.send(embed=embed)
+    except Exception as e:
+        print(f"Error in removelog command: {e}")
+        traceback.print_exc()
+        error_embed = discord.Embed(
+            title="❌ Error",
+            description=f"An error occurred while removing the logging channel: {str(e)}",
+            color=discord.Color.red()
+        )
+        await ctx.send(embed=error_embed)
+
+@client.event
+async def on_message(message):
+    try:
+        # Only process commands, don't log messages
+        await client.process_commands(message)
+    except Exception as e:
+        print(f"Error processing message: {e}")
+        traceback.print_exc()
+
+@client.event
+async def on_message_delete(message):
+    try:
+        if message.author == client.user:
+            return
+            
+        # Get logging channel for this server
+        if message.guild.id not in SERVER_LOGGING_CHANNELS:
+            return  # Skip if no logging channel set for this server
+            
+        channel = client.get_channel(SERVER_LOGGING_CHANNELS[message.guild.id])
+        if channel is None:
+            print(f"Error: Logging channel {SERVER_LOGGING_CHANNELS[message.guild.id]} not found!")
+            return
         
+        embed = discord.Embed(
+            title="Message Deleted",
+            color=discord.Color.red(),
+            timestamp=datetime.datetime.now(pytz.timezone('Asia/Ho_Chi_Minh'))
+        )
+        
+        embed.add_field(name="Author", value=message.author, inline=True)
+        embed.add_field(name="Channel", value=message.channel, inline=True)
+        
+        # Handle attachments in deleted message
+        if message.attachments:
+            for attachment in message.attachments:
+                try:
+                    async with aiohttp.ClientSession() as session:
+                        async with session.get(attachment.url) as resp:
+                            if resp.status == 200:
+                                data = io.BytesIO(await resp.read())
+                                await channel.send(
+                                    f"Deleted file from {message.author} in {message.channel}",
+                                    file=discord.File(data, attachment.filename)
+                                )
+                except Exception as e:
+                    print(f"Error downloading deleted attachment: {e}")
+                    traceback.print_exc()
+        
+        if message.content:
+            embed.add_field(name="Content", value=message.content, inline=False)
+        
+        await channel.send(embed=embed)
+    except Exception as e:
+        print(f"Error in on_message_delete: {e}")
+        traceback.print_exc()
 
-# def time_string():
-#     tz_NY = pytz.timezone('Asia/Ho_Chi_Minh')
-#     datetime_NY = datetime.now(tz_NY)
-#     return datetime_NY.strftime("%m/%d/%Y, %H:%M:%S")
+@client.event
+async def on_message_edit(before, after):
+    try:
+        if before.author == client.user:
+            return
+            
+        if before.content == after.content:
+            return
+            
+        # Get logging channel for this server
+        if before.guild.id not in SERVER_LOGGING_CHANNELS:
+            return  # Skip if no logging channel set for this server
+            
+        channel = client.get_channel(SERVER_LOGGING_CHANNELS[before.guild.id])
+        if channel is None:
+            print(f"Error: Logging channel {SERVER_LOGGING_CHANNELS[before.guild.id]} not found!")
+            return
+        
+        embed = discord.Embed(
+            title="Message Edited",
+            color=discord.Color.yellow(),
+            timestamp=datetime.datetime.now(pytz.timezone('Asia/Ho_Chi_Minh'))
+        )
+        
+        embed.add_field(name="Author", value=before.author, inline=True)
+        embed.add_field(name="Channel", value=before.channel, inline=True)
+        embed.add_field(name="Before", value=before.content, inline=False)
+        embed.add_field(name="After", value=after.content, inline=False)
+        
+        # Handle attachments in edited message
+        if before.attachments:
+            for attachment in before.attachments:
+                try:
+                    async with aiohttp.ClientSession() as session:
+                        async with session.get(attachment.url) as resp:
+                            if resp.status == 200:
+                                data = io.BytesIO(await resp.read())
+                                await channel.send(
+                                    f"Edited file from {before.author} in {before.channel}",
+                                    file=discord.File(data, attachment.filename)
+                                )
+                except Exception as e:
+                    print(f"Error downloading edited attachment: {e}")
+                    traceback.print_exc()
+        
+        await channel.send(embed=embed)
+    except Exception as e:
+        print(f"Error in on_message_edit: {e}")
+        traceback.print_exc()
 
-client.run("OTI0Njc1MjQ4ODcwMjczMDU2.GxcDT1._YQnpHbjxHlWKM5aMiYDujNnC_theL52k0jY0o")
+# Get token from environment variable
+TOKEN = os.getenv('DISCORD_TOKEN')
+if not TOKEN:
+    raise ValueError("No token found. Please set DISCORD_TOKEN in .env file")
+
+client.run(TOKEN)
